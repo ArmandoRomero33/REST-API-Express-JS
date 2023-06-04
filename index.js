3318da2b182e7e4dc53d9522327252424dfb24df
Jose Armando Romero <armamdoromeroorbe33@gmail.com>
2023-06-04 14:48:39 -0600

    Routing con Express

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hola, mundo')
+});
+
+app.get('/nueva ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta')
+});
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'producto 1',
+    price: '200'
+   });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
