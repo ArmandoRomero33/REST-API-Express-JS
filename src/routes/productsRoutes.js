04591ff6ea5eb7188b68c3d2b622b450177e13b3
Jose Armando Romero <armamdoromeroorbe33@gmail.com>
2023-06-05 21:58:35 -0600

    Codigos de Estado O HTTP

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,60 +1,43 @@
 const express = require('express');
-const {faker} = require('@faker-js/faker');
+const ProductService = require('../services/productService');
 
 const router = express.Router();
+const service = new ProductService();
 //get ----------
-router.get('/', (req, res) => {
-  const products = []
-  const {size} = req.query
-  const limit = size || 10
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.product(),
-      price: faker.commerce.price(),
-      image: faker.image.imageUrl()
-    })
-  }
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products)
 })
-router.get('/filter', (req, res) => {
-  res.send('Esto es un filtro!')
-})
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const {id} = req.params
-  res.json({
-    name: faker.commerce.product(),
-    price: faker.commerce.price(),
-    image: faker.image.imageUrl()
-  })
+  const product = await service.findOne(id)
+  res.json(product)
 })
 
 //post ----------
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const body = req.body
-  res.json({
-    message: 'created',
-    data: body
-  })
+  const product = await service.create(body)
+  res.status(201).json(product)
 })
 
 //patch ----------
-router.patch('/:id', (req, res) => {
-  const {id} = req.params
-  const body = req.body
-  res.json({
-    message: 'updated',
-    data: body,
-    id
-  })
+router.patch('/:id', async (req, res) => {
+  try {
+    const {id} = req.params
+    const body = req.body
+    const product = await service.update(id, body)
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(404).json({message: error.message})
+  }
 })
 
 //delete ----------
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const {id} = req.params
-  res.json({
-    message: 'deleted',
-    id
-  })
+  const product = await service.delete(id)
+  product ? res.status(200).json(product) : res.status(404).json({message: 'not found'})
 })
 
 module.exports = router
---
+++ src/services/productService.js
@@ -0,0 +1,63 @@
+const faker = require("faker")
+
+class ProductService {
+
+    constructor(){
+        this.products = []
+        this.generate()
+    }
+
+    async generate(){
+        const limit = 100
+        for (let i = 0; i < limit; i++) {
+            this.products.push({
+                id: faker.datatype.uuid(),
+                name: faker.commerce.product(),
+                price: faker.commerce.price(),
+                image: faker.image.imageUrl()
+            })
+        }
+    }
+
+    async create(data){
+        const newProduct = {
+            id: faker.datatype.uuid(),
+            ...data
+        }
+        this.products.push(newProduct)
+        return newProduct
+    }
+
+    async find(){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(this.products)
+            }, 1000)
+        })
+    }
+
+    async findOne(id){
+        return this.products.find(product => product.id === id)
+    }
+
+    async update(id, data){
+        const index = this.products.findIndex(product => product.id === id)
+        if(index === -1){
+            throw new Error('Product not found')
+        }
+        const product = this.products[index]
+        this.products[index] = {id, product, ...data}
+        return this.products[index]
+    }
+
+    async delete(id){{
+        const index = this.products.findIndex(product => product.id === id)
+        if(index === -1){
+            return null
+        }
+        this.products.splice(index, 1)
+        return {message: 'deleted'}
+    }}
+}
+
+module.exports = ProductService
